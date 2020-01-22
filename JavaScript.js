33f500f3de6c0d6b7b487c6855b2e4476db2dc09
Andrew Alforov <andrevpost@gmail.com>
2020-01-22 20:53:32 +0200

    js edited

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,28 @@
 ﻿let src = 'https://poloniex.com/public?command=returnCurrencies';
+
+function json2table(json, classes) {
+    var cols = Object.keys(json[0]);
+    var headerRow = '';
+    var bodyRows = '';
+    classes = classes || '';
+    cols.map(function (col) {
+        headerRow += '<th>' + col + '</th>';
+    });
+    json.map(function (row) {
+        bodyRows += '<tr>';
+
+        cols.map(function (colname) {
+            bodyRows += '<td>' + row[colname] + '</td>';
+        })
+
+        bodyRows += '</tr>';
+    });
+
+    return '<table class="' + classes + '"><thead><tr>' + headerRow + '</tr></thead><tbody>' + bodyRows + '</tbody></table>';
+
+    var defaultData = [
+        name: '1',
+        humanType: '2',
+
+    ]
+}
